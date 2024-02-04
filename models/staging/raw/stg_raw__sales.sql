f132cf46236ac347dd63fdcfea27ebd30293b7ca
Zein Moualla <zeinga1400@gmail.com>
2024-02-04 15:45:53 +0000

    Made tests

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -13,7 +13,8 @@ renamed as (
         orders_id,
         pdt_id AS products_id,
         revenue,
-        quantity
+        quantity,
+        CONCAT(pdt_id, '-', orders_id) AS pk
 
     from source
 
