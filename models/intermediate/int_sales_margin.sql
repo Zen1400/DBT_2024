d0c656985858b3e87a35e499d6b841a00e7f0904
Zein Moualla <zeinga1400@gmail.com>
2024-12-17 16:54:00 +0000

    dsqdsq

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -2,8 +2,7 @@ SELECT orders_id, date_date, sales.pdt_id,
  quantity * CAST(purchase_price AS FLOAT64) AS purchase_cost,
 revenue,
 revenue - (quantity * CAST(purchase_price AS FLOAT64)) AS margin, 
-quantity,
-{{ margin_percent('revenue', 'quantity * CAST(purchase_price AS FLOAT64)')}} AS margin_percent
+quantity
 FROM {{ ref('stg_raw__sales')}} AS sales
 LEFT JOIN {{ ref('stg_raw__product')}} AS products
 ON sales.pdt_id = products.products_id
